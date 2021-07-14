41646de55667f839ad181884ed0432f7cf4d0d7b
Massimo Vassalli <massimo.vassalli@gmail.com>
2021-07-14 19:11:59 +0100

    publication

---
+++ indexbbc6.php
@@ -0,0 +1,79 @@
+<br />
+<b>Warning</b>:  fopen(/home/httpd/html/legocms/tmp/cache/contentcache.php) [<a href='function.fopen'>function.fopen</a>]: failed to open stream: Permission denied in <b>/home/httpd/html/legocms/lib/classes/class.contentoperations.inc.php</b> on line <b>549</b><br />
+<br />
+<b>Warning</b>:  fwrite(): supplied argument is not a valid stream resource in <b>/home/httpd/html/legocms/lib/classes/class.contentoperations.inc.php</b> on line <b>550</b><br />
+<br />
+<b>Warning</b>:  fclose(): supplied argument is not a valid stream resource in <b>/home/httpd/html/legocms/lib/classes/class.contentoperations.inc.php</b> on line <b>551</b><br />
+<?xml version="1.0"?>
+<rss version="2.0">
+	<channel>
+		<title>Scienza Ludica</title>
+		<link>http://www.scienzaludica.it</link>
+		<description>Current News entries</description>
+				<item>
+			<title><![CDATA[First Lego League a Pistoia]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=9&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=9&amp;cntnt01returnid=102</guid>
+			<pubDate>Tue, 14 Dec 2010 18:27:27 +0000</pubDate>
+			<category><![CDATA[Eventi]]></category>
+			<description><![CDATA[First Lego League a Pistoia - sabato 21 febbraio 20153° Concorso Internazionale di Scienza e Robotica - Qualificazione Centro Italia
+			La qualificazione valida per il centro Italia si terrà il 21 Febbrai, con inizio alle ore 9:00presso l'Auditorium Provinciale di Pistoia (Via dei Panconi 12, Pistoia). Vedi il programma completo  Sito WebITTS Fedi-Fermi, Via Panconi, 3951100 - Pistoia - Italia Contatto prof. Riccardo Niccolaimail: r.niccolai@gmail.com]]></description>
+		</item>
+				<item>
+			<title><![CDATA[Festival della Scienza 2010]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=8&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=8&amp;cntnt01returnid=102</guid>
+			<pubDate>Fri, 29 Oct 2010 07:31:54 +0000</pubDate>
+			<category><![CDATA[General]]></category>
+			<description><![CDATA[Guardare oltre il visibile 
+			Il laboratorio di Scienzaludica al Festival della Scienza 2010 di GenovaIl molto piccolo e il molto grande... seduti su un mattoncino LEGOSpingere lo sguardo verso gli orizzonti più lontani, osservare oggetti dalle piccole dimensioni. Per comprendere i fenomeni naturali, gli scienziati di ogni tempo hanno costruito strumenti in grado di potenziare la loro vista per andare oltre i limiti dei propri occhi, verso il molto grande e il molto piccolo. Oggi, tecnologie sempre più sofisticate permettono di esplorare la natura come mai è stato possibile: i nostri telescopi si spingono fino ai confini dell'Universo, mentre la microscopia arriva a distinguere gli atomi che formano la materia. Eppure, con un po' di LEGO anche noi possiamo costruire il nostro microscopio e osservare alcuni campioni che mostrano aspetti inattesi della materia. Ma come funziona il tutto? Com'è possibile ingrandire le immagini? I banchi ottici permettono ai visitatori di capire qual è il sistema ottico alla base del microscopio e di comprendere il funzionamento di un  telescopio. Alcuni esperimenti con questo strumento evidenziano inoltre gli inevitabili ostacoli posti ai telescopi dall'atmosfera terrestre, che causa delle aberrazioni ottiche (cosa che spiega i vantaggi di collocare i telescopi nello spazio). Un affascinante percorso nella fisica della visione per sperimentare, per toccare con mano e conoscere le tecnologie che permettono andare oltre il visibile.]]></description>
+		</item>
+				<item>
+			<title><![CDATA[ESOF 2010]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=6&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=6&amp;cntnt01returnid=102</guid>
+			<pubDate>Mon, 31 May 2010 08:29:08 +0000</pubDate>
+			<category><![CDATA[General]]></category>
+			<description><![CDATA[Scienzaludica.IT parteciperà all'edizione 2010 dello European Science Open Forum che si terrà a Torino dal 2 al 7 Luglio 2010.
+			 La televisione realizzata con il LEGO in ricordo delle prime televisioni meccaniche verrà portata a Torino, nello stand del CNR. ]]></description>
+		</item>
+				<item>
+			<title><![CDATA[Festival della Scienza 2009]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=5&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=5&amp;cntnt01returnid=102</guid>
+			<pubDate>Sun, 01 Nov 2009 09:18:09 +0000</pubDate>
+			<category><![CDATA[Eventi]]></category>
+			<description><![CDATA[Scienzaludica partecipa all'edizione 2009 del Festival della Scienza di Genova
+			Dal 23 Ottobre al primo Novembre 2009 Scienzaludica ha approntato un laboratorio presso il Festival della Scienza di Genova. Il laboratorio, denominato SCIENZALUDICA.IT ha portato il visitatore a toccare con mano il funzionamento di dispositivi oggigiorno sempre più diffusi, come l'auto-focus di una macchina fotografica o la televisione, e a confrontarsi con le tecniche di visualizzazione tridimensionale, il vero futuro della comunicazione interattiva e della tecnologia ottica, il tutto sempre mediato dai mattoncini LEGO. Visualizza le foto dell'evento.]]></description>
+		</item>
+				<item>
+			<title><![CDATA[congresso SIF]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=4&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=4&amp;cntnt01returnid=102</guid>
+			<pubDate>Tue, 30 Dec 2008 09:20:37 +0000</pubDate>
+			<category><![CDATA[Eventi]]></category>
+			<description><![CDATA[Congresso SIFScienza ludica: impariamo con il LEGOGiacomelli G., Quercioli F., Puccioni G., Tiribilli B., Vassalli M., Mati F.
+			ISC - Istituto Sistemi Complessi del CNR, Sezione TD di FirenzeITTI s.a.s., FirenzeSono più di dieci anni ormai che, con alterne vicende, &quot;giochiamo&quot; con il LEGO escogitando nuovi esperimenti per invogliare studenti d'ogni ordine e grado ad avvicinarsi alla scienza in modo divertente. Era il 1997 quandopartecipammo al &quot;Fifth International Topical Meeting on Education and training in optics&quot;, con una comunicazione dal titolo: &quot;Play Optics with LEGO&quot;. The Daily Telegraph e The Guardian scrissero un articolo sul nostro lavoro e diverse riviste ci dedicarono la copertina.Con il contributo del MIUR è adesso disponibile un sito internet dedicato a questa attività di diffusione della cultura scientifica e tecnica attraverso il gioco.Scarica la presentazione (2 Mb- PDF)   &nbsp;]]></description>
+		</item>
+				<item>
+			<title><![CDATA[RoboLego project]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=3&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=3&amp;cntnt01returnid=102</guid>
+			<pubDate>Thu, 18 Dec 2008 15:26:25 +0000</pubDate>
+			<category><![CDATA[Eventi]]></category>
+			<description><![CDATA[RoboLego projectWe are writing to offer you the possibility of participating in a free European refresher course aimed at all teachers who wish to learn a new...
+			We are writing to offer you the possibility of participating in a free European refresher course aimed at all teachers who wish to learn a new didactic methodology using play as a way of learning. Attached herewith, you will find a complete description of the course. It will be held in Italy, in Pistoia (a Tuscan city close to Florence) from 03/04/2009 to 10/04/2009. The course is financed by the European Union, as it is a Comenius/Grundtvig In-Service Training for teachers. The deadline for application is January 2009; it should be filled in adhering to the regulations set by your National Agency and should be signed by the Headmaster of your school. This course seeks to develop understanding of and between various European cultures through exchanges and co-operation between schools in different countries. We believe that this could be an important way for teachers from the EU to exchange experience. [more] ]]></description>
+		</item>
+				<item>
+			<title><![CDATA[Festival della creatività]]></title>
+			<link>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=2&amp;cntnt01returnid=102</link>
+			<guid>http://www.scienzaludica.it/index.php?mact=News,cntnt01,detail,0&amp;cntnt01articleid=2&amp;cntnt01returnid=102</guid>
+			<pubDate>Wed, 29 Oct 2008 15:01:35 +0000</pubDate>
+			<category><![CDATA[General]]></category>
+			<description><![CDATA[OpenLab - Festival della CretivitàScienza Ludica partecipa al Festival della Creatività
+			L'attività di Scienza Ludica è stata presentata all'interno di uno spazio di Open Lab scarica la brochure ( 12 Mb - PDF)scarica la locandina ( 2644 Kb - JPG ) <a href="http://www.scienzaludica.it/index.php?page=openlab" title="OpenLab">Galleria fotografica</a> ]]></description>
+		</item>
+			</channel>
+</rss> 
+<!-- Generated in 0.147287 seconds by CMS Made Simple using 11 SQL queries -->
+<!-- CMS Made Simple - Released under the GPL - http://cmsmadesimple.org -->
